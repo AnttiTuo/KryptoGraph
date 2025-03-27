7e8bb4373719cfd9a5a2f2acaaaaa4f134239e46
Wizartti <anttituomela.xd@gmail.com>
2025-03-27 02:49:19 +0200

    Crypto

---
+++ KryptoGraph1/KryptoGraph/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace KryptoGraph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkedListBoxCryptos = new System.Windows.Forms.CheckedListBox();
+            this.buttonAddCrypto = new System.Windows.Forms.Button();
+            this.textBoxCrypto = new System.Windows.Forms.TextBox();
+            this.Info = new System.Windows.Forms.Label();
+            this.labelCryptoPrices = new System.Windows.Forms.Label();
+            this.listBoxPriceTracker = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.chart1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.HorizontalCenter;
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chart1.BackHatchStyle = System.Windows.Forms.DataVisualization.Charting.ChartHatchStyle.Percent50;
+            this.chart1.BorderlineColor = System.Drawing.Color.Black;
+            chartArea1.BackColor = System.Drawing.Color.White;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.Visible = false;
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 156);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "BTC";
+            series1.YValuesPerPoint = 4;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1208, 534);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.BackColor = System.Drawing.Color.AliceBlue;
+            title1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            title1.BorderWidth = 0;
+            title1.Font = new System.Drawing.Font("Sylfaen", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            title1.Name = "Title1";
+            title1.ShadowOffset = 1;
+            title1.Text = "Crypto Prices";
+            title1.TextStyle = System.Windows.Forms.DataVisualization.Charting.TextStyle.Shadow;
+            this.chart1.Titles.Add(title1);
+            // 
+            // checkedListBoxCryptos
+            // 
+            this.checkedListBoxCryptos.CheckOnClick = true;
+            this.checkedListBoxCryptos.FormattingEnabled = true;
+            this.checkedListBoxCryptos.Items.AddRange(new object[] {
+            "BTCEUR",
+            "ETHEUR",
+            "DOGEEUR",
+            "XRPEUR",
+            "SHIBEUR"});
+            this.checkedListBoxCryptos.Location = new System.Drawing.Point(12, 10);
+            this.checkedListBoxCryptos.Name = "checkedListBoxCryptos";
+            this.checkedListBoxCryptos.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBoxCryptos.TabIndex = 2;
+            // 
+            // buttonAddCrypto
+            // 
+            this.buttonAddCrypto.Location = new System.Drawing.Point(162, 117);
+            this.buttonAddCrypto.Name = "buttonAddCrypto";
+            this.buttonAddCrypto.Size = new System.Drawing.Size(170, 33);
+            this.buttonAddCrypto.TabIndex = 3;
+            this.buttonAddCrypto.Text = "Add";
+            this.buttonAddCrypto.UseVisualStyleBackColor = true;
+            // 
+            // textBoxCrypto
+            // 
+            this.textBoxCrypto.Location = new System.Drawing.Point(148, 84);
+            this.textBoxCrypto.Name = "textBoxCrypto";
+            this.textBoxCrypto.Size = new System.Drawing.Size(195, 20);
+            this.textBoxCrypto.TabIndex = 4;
+            // 
+            // Info
+            // 
+            this.Info.AutoSize = true;
+            this.Info.Location = new System.Drawing.Point(145, 68);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(283, 13);
+            this.Info.TabIndex = 5;
+            this.Info.Text = "Write your crypto abbreviation and currency (ex. BTCEUR)";
+            // 
+            // labelCryptoPrices
+            // 
+            this.labelCryptoPrices.AutoSize = true;
+            this.labelCryptoPrices.Font = new System.Drawing.Font("Roboto Condensed", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCryptoPrices.Location = new System.Drawing.Point(503, 9);
+            this.labelCryptoPrices.Name = "labelCryptoPrices";
+            this.labelCryptoPrices.Size = new System.Drawing.Size(40, 18);
+            this.labelCryptoPrices.TabIndex = 6;
+            this.labelCryptoPrices.Text = "Price";
+            // 
+            // listBoxPriceTracker
+            // 
+            this.listBoxPriceTracker.FormattingEnabled = true;
+            this.listBoxPriceTracker.Location = new System.Drawing.Point(506, 30);
+            this.listBoxPriceTracker.Name = "listBoxPriceTracker";
+            this.listBoxPriceTracker.Size = new System.Drawing.Size(257, 95);
+            this.listBoxPriceTracker.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 691);
+            this.Controls.Add(this.listBoxPriceTracker);
+            this.Controls.Add(this.labelCryptoPrices);
+            this.Controls.Add(this.Info);
+            this.Controls.Add(this.textBoxCrypto);
+            this.Controls.Add(this.buttonAddCrypto);
+            this.Controls.Add(this.checkedListBoxCryptos);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.CheckedListBox checkedListBoxCryptos;
+        private System.Windows.Forms.Button buttonAddCrypto;
+        private System.Windows.Forms.TextBox textBoxCrypto;
+        private System.Windows.Forms.Label Info;
+        private System.Windows.Forms.Label labelCryptoPrices;
+        private System.Windows.Forms.ListBox listBoxPriceTracker;
+    }
+}
+
---
+++ KryptoGraph1/KryptoGraph/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Linq;
+
+namespace KryptoGraph
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private Timer priceUpdateTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+
+            // Lisää tapahtumankäsittelijä kryptojen lisäämisnapille
+            buttonAddCrypto.Click += buttonAddCrypto_Click;
+
+            // Lisää tapahtumankäsittelijä valintalaatikon muutoksille
+            checkedListBoxCryptos.ItemCheck += CheckedListBoxCryptos_ItemCheck;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Alustetaan ajastin (päivitetään hinnat 30 sekunnin välein)
+            priceUpdateTimer = new Timer();
+            priceUpdateTimer.Interval = 30000; // 30 sekuntia
+            priceUpdateTimer.Tick += async (s, ev) => await UpdateChart();
+            priceUpdateTimer.Start();
+
+            // Ladataan ensimmäinen valittu kryptovaluutta
+            checkedListBoxCryptos.SetItemChecked(0, true);
+            _ = UpdateChart(); // Haetaan aloitustiedot
+        }
+
+        // Käsittelee, kun käyttäjä valitsee tai poistaa krypton listasta
+        private async void CheckedListBoxCryptos_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            await Task.Delay(100); // Pieni viive UI-päivitykselle
+
+            string symbol = checkedListBoxCryptos.Items[e.Index].ToString();
+
+            if (e.NewValue == CheckState.Checked)
+            {
+                await UpdateChart(); // ✅ Lisää krypto
+            }
+            else
+            {
+                RemoveCryptoFromChart(symbol); // ✅ Poista krypto
+            }
+
+            await UpdatePriceTracker(); // Päivitä hintaseuranta
+        }
+
+        // Päivittää kaavion ja hinnat kaikille valituille kryptovaluutoille
+        private async Task UpdateChart()
+        {
+            chart1.Series.Clear(); // Tyhjennetään vanhat tiedot
+
+            var checkedCryptos = checkedListBoxCryptos.CheckedItems.Cast<string>().ToList();
+
+            foreach (var symbol in checkedCryptos)
+            {
+                var priceData = await FetchBinanceKlines(symbol);
+                if (priceData != null)
+                {
+                    AddToChart(priceData, symbol);
+                }
+            }
+
+            await UpdatePriceTracker(); // Päivitä hintatracker
+        }
+
+        // Hakee historialliset hintatiedot Binance API:sta
+        private async Task<List<(DateTime, double)>> FetchBinanceKlines(string symbol)
+        {
+            try
+            {
+                string url = $"https://api.binance.com/api/v3/klines?symbol={symbol}&interval=1m&limit=1440";
+                string json = await client.GetStringAsync(url);
+                dynamic data = JsonConvert.DeserializeObject(json);
+
+                var prices = new List<(DateTime, double)>();
+                foreach (var entry in data)
+                {
+                    DateTime time = DateTimeOffset.FromUnixTimeMilliseconds((long)entry[0]).DateTime;
+                    double price = Convert.ToDouble(entry[4].ToString(), CultureInfo.InvariantCulture);
+                    prices.Add((time, price));
+                }
+
+                return prices;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Virhe haettaessa tietoja kryptolle {symbol}: " + ex.Message);
+                return null;
+            }
+        }
+
+        // Lisää uusi krypto listaan ja päivittää kaavion
+        private void buttonAddCrypto_Click(object sender, EventArgs e)
+        {
+            string newSymbol = textBoxCrypto.Text.Trim().ToUpper();
+
+            if (string.IsNullOrWhiteSpace(newSymbol))
+            {
+                MessageBox.Show("Anna kelvollinen kryptovaluutan symboli.", "Virhe", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!checkedListBoxCryptos.Items.Contains(newSymbol)) // Vältetään duplikaatit
+            {
+                checkedListBoxCryptos.Items.Add(newSymbol, true);
+                _ = UpdateChart();
+            }
+            else
+            {
+                MessageBox.Show("Tämä kryptovaluutta on jo listassa.", "Varoitus", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            textBoxCrypto.Clear(); // Tyhjennä tekstikenttä
+        }
+
+        // Poistaa krypton kaaviosta
+        private void RemoveCryptoFromChart(string symbol)
+        {
+            if (chart1.Series.Any(s => s.Name == symbol))
+            {
+                chart1.Series.Remove(chart1.Series[symbol]);
+            }
+
+            string chartAreaName = $"ChartArea_{symbol}";
+
+            if (chart1.ChartAreas.Any(ca => ca.Name == chartAreaName))
+            {
+                chart1.ChartAreas.Remove(chart1.ChartAreas[chartAreaName]);
+            }
+
+            chart1.Refresh();
+        }
+
+        // Lisää krypton hintatiedot kaavioon
+        private void AddToChart(List<(DateTime, double)> data, string symbol)
+        {
+            if (chart1.Series.IndexOf(symbol) != -1)
+            {
+                chart1.Series.Remove(chart1.Series[symbol]);
+            }
+
+            var series = new Series
+            {
+                Name = symbol,
+                ChartType = SeriesChartType.Line,
+                XValueType = ChartValueType.DateTime
+            };
+
+            foreach (var point in data)
+            {
+                series.Points.AddXY(point.Item1, point.Item2); // ✅ Käytä Item1 (aika) ja Item2 (hinta)
+            }
+
+            chart1.Series.Add(series);
+
+            string chartAreaName = $"ChartArea_{symbol}";
+
+            if (!chart1.ChartAreas.Any(ca => ca.Name == chartAreaName))
+            {
+                var newChartArea = new ChartArea(chartAreaName);
+                newChartArea.AxisX.LabelStyle.Format = "HH:mm";
+                newChartArea.AxisX.MajorGrid.Enabled = false;
+                newChartArea.AxisY.MajorGrid.Enabled = false;
+                newChartArea.AxisY.LabelStyle.Format = "0.########";
+                chart1.ChartAreas.Add(newChartArea);
+            }
+
+            series.ChartArea = chartAreaName;
+
+            foreach (var chartArea in chart1.ChartAreas)
+            {
+                chartArea.RecalculateAxesScale();
+            }
+        }
+
+        // 🔹 **Tunnistaa kryptovaluutan valuutan nimen perusteella**
+        private string GetCurrencyFromSymbol(string symbol)
+        {
+            string[] knownCurrencies = { "USDT", "EUR", "USD", "GBP", "JPY", "BUSD", "USDC" };
+
+            foreach (var currency in knownCurrencies)
+            {
+                if (symbol.EndsWith(currency))
+                {
+                    return currency;
+                }
+            }
+
+            return "Tuntematon"; // Jos ei tunnisteta
+        }
+
+        // 🔹 **Päivittää hintaseurannan (ListBox)**
+        private async Task UpdatePriceTracker()
+        {
+            // Kopioidaan valitut kryptot listaan ennen silmukointia, jotta CheckedItems ei muutu kesken iteraation
+            var checkedCryptos = checkedListBoxCryptos.CheckedItems.Cast<string>().ToList();
+
+            // Päivitetään hinnat taustasäikeessä, jotta UI ei jäädy
+            await Task.Run(async () =>
+            {
+                var newItems = new List<string>(); // Uusi lista, jotta vältetään UI:n suora muokkaus silmukan aikana
+
+                foreach (var symbol in checkedCryptos)
+                {
+                    var priceData = await FetchBinanceKlines(symbol);
+                    if (priceData == null || priceData.Count == 0)
+                        continue;
+
+                    string currency = GetCurrencyFromSymbol(symbol);
+                    double latestPrice = priceData.Last().Item2;
+
+                    newItems.Add($"{symbol}: {latestPrice} {currency}");
+                }
+
+                // Päivitetään UI-pääsäikeessä
+                Invoke(new Action(() =>
+                {
+                    listBoxPriceTracker.Items.Clear();
+                    foreach (var item in newItems)
+                    {
+                        listBoxPriceTracker.Items.Add(item);
+                    }
+                }));
+            });
+        }
+
+    }
+}
